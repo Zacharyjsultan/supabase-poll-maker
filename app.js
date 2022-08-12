f4cb7a468447171c95ebdc594e55c2376f23a351
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-08-12 11:45:22 -0700

    added form/display current poll function / set up connection to supabase

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
 // import functions and grab DOM elements
+const createForm = document.getElementById('start-poll');
+const voteA = document.getElementById('votes-a');
+const voteB = document.getElementById('votes-b');
 
 // let state
+let question = '';
+let optionA = '';
+let optionB = '';
+let optionVotesA = 0;
+let optionVotesB = 0;
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+createForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = new FormData(createForm);
+
+    question = data.get('question-start');
+    optionA = data.get('option-a');
+    optionB = data.get('option-b');
+
+    displayCurrentPoll();
+});
+
+function displayCurrentPoll() {
+console.log(displayCurrentPoll);
+    const questionEl = document.getElementById('id');
+    questionEl.textContent = question;
+    const optionAEl = document.getElementById('option-a');
+    optionAEl.textContent = optionA;
+    const optionBEl = document.getElementById('option-b');
+    optionBEl.textContent = optionB;
+
+    voteA.textContent = optionVotesA;
+    voteB.textContent = optionVotesB;
+}
---
+++ fetch-utils.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = 'https://ttldeenxxjworwkqykdt.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0bGRlZW54eGp3b3J3a3F5a2R0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTk2NDc5MTMsImV4cCI6MTk3NTIyMzkxM30.jEFkrL00S4lZ9cr9E2YhS5KP-GPwcdDh89zvFcsM0kg';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
